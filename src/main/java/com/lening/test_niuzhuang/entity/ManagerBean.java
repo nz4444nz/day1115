62434a70f9b6ec9acf2916c30abb13ef842a33a4
nz <nz15735902698@gmail.com>
2022-11-16 09:14:10 +0800

    [A]新建工程

--- src/main/java/com/lening/test_niuzhuang/entity/ManagerBean.java
+++ src/main/java/com/lening/test_niuzhuang/entity/ManagerBean.java
@@ -1,9 +1,12 @@
 package com.lening.test_niuzhuang.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Table(name = "tb_manager")
 @Entity
+@Data
 public class ManagerBean {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -11,36 +14,5 @@ public class ManagerBean {
     private String username;
     private String password;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "ManagerBean{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
